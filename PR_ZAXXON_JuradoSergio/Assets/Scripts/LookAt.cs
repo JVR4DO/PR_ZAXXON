5534431aaf9ec4cadbdab349cd9d58f147c45467
Sergio <Sergiojurado111@gmail.com>
2021-10-01 19:20:21 +0200

    Seguimiento de la cámara al coche
    En playerPosition.position.y+2.37 lo he puesto porque quiero que la cámara empiece más arriba

---
+++ PR_ZAXXON_JuradoSergio/Assets/Scripts/LookAt.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    [SerializeField] Transform playerPosition;
+    //Variables necesarias para la opción de suavizado
+    [SerializeField] float smoothVelocity = 0.2F;
+    [SerializeField] Vector3 camaraVelocity = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = new Vector3(playerPosition.position.x, playerPosition.position.y+2.37f, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref camaraVelocity, smoothVelocity);
+
+    }
+ 
+}
