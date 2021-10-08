6c5b85929b503d631a69a34959a22d1a16337657
Sergio <Sergiojurado111@gmail.com>
2021-10-08 17:44:47 +0200

    Script para velocidad edificios
    Script para que el script del movimiento de los edificios entre a el

---
+++ PR_ZAXXON_JuradoSergio/Assets/Scripts/InicioJuego.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InicioJuego : MonoBehaviour
+{
+    public float juegoSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        juegoSpeed = 30f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
